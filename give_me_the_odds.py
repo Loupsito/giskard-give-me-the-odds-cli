b1fc9076c66c3291b648c0d36da5bfaf804f7b9b
AlexandreS <virya.suy@gmail.com>
2023-03-05 19:00:10 +0100

    feat: add POST request to call backend

--- give_me_the_odds.py
+++ give_me_the_odds.py
@@ -2,6 +2,8 @@ import argparse
 import json
 from typing import Dict, Any
 
+import requests
+
 
 def read_json(file_path: str) -> Dict[str, Any]:
     with open(file_path, 'r') as f:
@@ -11,17 +13,22 @@ def read_json(file_path: str) -> Dict[str, Any]:
 
 def main():
     parser = argparse.ArgumentParser(description='Read 2 JSON files')
-
     parser.add_argument('file1', type=str, help='path to the first JSON file')
     parser.add_argument('file2', type=str, help='path to the second JSON file')
-
     args = parser.parse_args()
 
-    json1 = read_json(args.file1)
-    json2 = read_json(args.file2)
+    millennium_falcon_json = read_json(args.file1)
+    empire_json = read_json(args.file2)
 
-    print(json1)
-    print(json2)
+    url = "http://localhost:8000/give_me_the_odds"
+    body = {
+        "millennium_falcon": millennium_falcon_json,
+        "empire": empire_json
+    }
+
+    response = requests.post(url, json=body)
+    result = response.json()['value']
+    print(result)
 
 
 if __name__ == '__main__':
